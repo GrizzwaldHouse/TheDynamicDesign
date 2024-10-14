3ccb8b0a1543f6f2d48fdaf879e0b4fc6a6cc6e4
Marcus Daley <MADaley@student.fullsail.edu>
2024-10-14 08:55:51 -0700

    TriggerSpawn point
    I changed spawn point from Input activated to trigger ,add to Prefab /Environment/Traps folder called it ObjectSpawnerTrigger.

--- UnityGroupProject/CryptCrawler/Assets/Scripts/RandomObjectSpawn.cs
+++ UnityGroupProject/CryptCrawler/Assets/Scripts/RandomObjectSpawn.cs
@@ -5,22 +5,28 @@ using UnityEngine;
 public class RandomObjectSpawn : MonoBehaviour
 
 
-{ public GameObject[] RandomObjects;
-    public SphereCollider trapCollider; // Assign this in the Inspector
-    private void OnTriggerEnter(Collider other)
+{
+    public Transform SpawnPoint;
+    public GameObject SpawnObject;
+    private void OnTriggerEnter()
     {
-        // Check if the player has entered the trap
-        if (other.gameObject.CompareTag("Player"))
-        {
-            SpawnRandomObject();
-        }
+        Instantiate(SpawnObject, SpawnPoint.position, SpawnPoint.rotation);
     }
-    private void SpawnRandomObject()
-    {
 
-        int randomIndex = Random.Range(0, RandomObjects.Length);
-        Vector3 randomSpawnPosition = new Vector3(Random.Range(-10, 11), 5, Random.Range(-10, 11));
-        Instantiate(RandomObjects[randomIndex], transform.position, Quaternion.identity);
-    }
-    
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    // Check if the player has entered the trap
+    //    if (other.gameObject.CompareTag("Player"))
+    //    {
+    //        SpawnRandomObject();
+    //    }
+    //}
+    //private void SpawnRandomObject()
+    //{
+
+    //    int randomIndex = Random.Range(0, RandomObjects.Length);
+    //    Vector3 randomSpawnPosition = new Vector3(Random.Range(-10, 11), 5, Random.Range(-10, 11));
+    //    Instantiate(RandomObjects[randomIndex], transform.position, Quaternion.identity);
+    //}
+
 }
