762b3c852df3b7bc6df562bb0aedd5730609a3b4
Marcus Daley <MADaley@student.fullsail.edu>
2024-10-23 00:03:36 -0700

    Did More designing
    Did more designing

--- UnityGroupProject/CryptCrawler/Assets/Scripts/MovingPlatform.cs
+++ UnityGroupProject/CryptCrawler/Assets/Scripts/MovingPlatform.cs
@@ -1,81 +1,80 @@
 using UnityEngine;
 
-//public class MovingPlatform : MonoBehaviour
-//{
+public class MovingPlatform : MonoBehaviour
+{
 
-//    // Serialized fields
-//    [SerializeField]
-//    private float _moveDistance;
+    // Serialized fields
+    [SerializeField]
+    private float _moveDistance;
 
-//    [SerializeField]
-//    private float _speed;
+    [SerializeField]
+    private float _speed;
 
-//    [SerializeField]
-//    private bool _loopMovement = true;
+    [SerializeField]
+    private bool _loopMovement = true;
 
-//    [SerializeField]
-//    private bool _resetOnCollision = true;
+    [SerializeField]
+    private bool _resetOnCollision = true;
 
-//    // Private fields
-//    private Vector3 _startPosition;
-//    private bool _isMoving = false;
-//    [SerializeField] private float _movementDirection;
+    // Private fields
+    private Vector3 _startPosition;
+    private bool _isMoving = false;
+    [SerializeField] private float _movementDirection;
 
-//    // Getters and setters
-//    public float MoveDistance
-//    {
-//        get { return _moveDistance; }
-//        set { _moveDistance = value; }
-//    }
+    // Getters and setters
+    public float MoveDistance
+    {
+        get { return _moveDistance; }
+        set { _moveDistance = value; }
+    }
 
-//    public float Speed
-//    {
-//        get { return _speed; }
-//        set { _speed = value; }
-//    }
+    public float Speed
+    {
+        get { return _speed; }
+        set { _speed = value; }
+    }
 
-//    public bool LoopMovement
-//    {
-//        get { return _loopMovement; }
-//        set { _loopMovement = value; }
-//    }
+    public bool LoopMovement
+    {
+        get { return _loopMovement; }
+        set { _loopMovement = value; }
+    }
 
-//    public bool ResetOnCollision
-//    {
-//        get { return _resetOnCollision; }
-//        set { _resetOnCollision = value; }
-//    }
+    public bool ResetOnCollision
+    {
+        get { return _resetOnCollision; }
+        set { _resetOnCollision = value; }
+    }
 
-//    void Start()
-//    {
-//        _startPosition = transform.position;
-//    }
-
-
+    void Start()
+    {
+        _startPosition = transform.position;
+    }
 
 
-//    void OnTriggerEnter(Collider other)
-//    {
-//        // Check if the character has landed on the platform
-//        if (other.gameObject.CompareTag("Player"))
-//        {
-//            // Debug.Log("Player has landed on the platform!");
-//            PlayerController.position = transform.position;
-//            // Set the flag to move the platform
-//            _isMoving = true;
-
-//            // Set the movement direction to 1 (right)
-//            _movementDirection = 1.0f;
-
-//            // If reset on collision is true, reset the platform position
-//            if (_resetOnCollision)
-//            {
-//                transform.position = _startPosition; // Reset platform position
-//            }
-//        }
-//    }
 
 
+    void OnTriggerEnter(Collider other)
+    {
+        // Check if the character has landed on the platform
+        if (other.gameObject.CompareTag("Player"))
+        {
+         
+            // Set the flag to move the platform
+            _isMoving = true;
+
+            // Set the movement direction to 1 (right)
+            _movementDirection = 1.0f;
+
+            // If reset on collision is true, reset the platform position
+            if (_resetOnCollision)
+            {
+                transform.position = _startPosition; // Reset platform position
+            }
+        }
+    }
+
+}
 
 
 //    void Update()
