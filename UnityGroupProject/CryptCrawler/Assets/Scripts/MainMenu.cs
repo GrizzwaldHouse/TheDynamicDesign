dd008aeca9640dc7bde5b80e65cc5ef2725b020b
Marcus Daley <MADaley@student.fullsail.edu>
2024-11-23 10:10:37 -0800

    Design Changes

--- UnityGroupProject/CryptCrawler/Assets/Scripts/MainMenu.cs
+++ UnityGroupProject/CryptCrawler/Assets/Scripts/MainMenu.cs
@@ -1,44 +1,53 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using TMPro;
-//using UnityEngine;
-//using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-//public class MainMenu : MonoBehaviour
-//{
-//    // Reference to the TextMeshProUGUI component that displays the high score
-//    public TMP_Text highScoreUI;
+public class MainMenu : MonoBehaviour
+{
+ 
+    public GameObject menu;
+    public GameObject loadingInterface;
+    public Image loadingProgressBar;
+    List<AsyncOperation> sceneToLoad =new List<AsyncOperation>();
+   
 
-//    // The name of the scene to load when starting a new game
-//    string newGameScene = "CryptCrawler";
+public void StartGame()
+    {
+        HideMenu();
+        ShowLoadingScreen();
+        sceneToLoad.Add(SceneManager.LoadSceneAsync("MainMenu"));
+        sceneToLoad.Add(SceneManager.LoadSceneAsync("CryptCrawler", LoadSceneMode.Additive));
+        StartCoroutine(LoadingScreen());
+    }
+    public void HideMenu()
+    {
+        menu.SetActive(false);
+    }
+    public void ShowLoadingScreen()
+    {
+        loadingInterface.SetActive(true);
+    }
+    IEnumerator LoadingScreen()
+    {
+        float totalProgress = 0;
+        for (int i = 0; i < sceneToLoad.Count; i++)
+        {
 
-//    // Called when the script is initialized
-//    public void Start()
-//    {
-//        // Load the high score from the SaveLoadManager instance
-//        int highScore = SaveLoadManager.Instance.LoadHighScore();
-
-//        // Update the high score UI text with the loaded high score
-//        highScoreUI.text = $"Top Score Survived: " + highScore;
-//    }
-
-//    // Called when the "Start New Game" button is clicked
-//    public void startNewGame()
-//    {
-//        // Load the new game scene using the SceneManager
-//        SceneManager.LoadScene(newGameScene);
-//    }
-
-//    // Called when the "Exit" button is clicked
-//    public void ExitApplication()
-//    {
-//        // If we're in the Unity Editor, stop playing the scene
-//#if UNITY_EDITOR
-//        UnityEditor.EditorApplication.isPlaying = false;
-
-//#else
-//        // If we're in a built application, quit the application
-//        Application.Quit(); 
-//#endif
-//    }
-//}
+            while (!sceneToLoad[i].isDone)
+            {
+                totalProgress += sceneToLoad[i].progress;
+                loadingProgressBar.fillAmount = totalProgress / sceneToLoad.Count;
+                yield return null;
+            }
+        }
+        
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
